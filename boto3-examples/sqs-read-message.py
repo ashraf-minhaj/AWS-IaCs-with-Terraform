f527298f047f50f76709a3546695f63a8bfc8b3d
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2023-01-11 05:21:29 +0600

    check if message present

--- boto3-examples/sqs-read-message.py
+++ boto3-examples/sqs-read-message.py
@@ -3,8 +3,8 @@ import boto3
 MAX_MESSAGE     = 1
 WAIT_INTERVAL   = 15
 
-queue = f"https://sqs.ap-southeast-1.amazonaws.com/owner-id/queue-name"
-
+# queue = f"https://sqs.ap-southeast-1.amazonaws.com/owner-id/queue-name"
+queue = "https://sqs.ap-southeast-1.amazonaws.com/669201380121/mars-mariners-jobs-dev.fifo"
 sqs_client = boto3.client("sqs", region_name="ap-southeast-1")
 
 response = sqs_client.receive_message(
@@ -16,8 +16,10 @@ response = sqs_client.receive_message(
 response_content = response.get('Messages', [])
 # num_of_msgs = len(response_content)
 print(response_content)
-
+print(type(response_content))
 # body has the actual content
 if response_content:
     message_body = response_content[0]['Body']
     print(message_body)
+elif not response_content:
+    print("No message")
