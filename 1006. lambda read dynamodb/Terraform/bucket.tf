044e4e73c166b1933258e6bd9b4b51feb1728991
Ashraf Minhaj <ashraf_minhaj@yahoo.com>
2024-10-29 15:51:27 +0600

    [housekeeping](unused): remove unnecessary files

--- 1006. lambda read dynamodb/Terraform/bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "s3_bucket" {
-    bucket = "${var.bucket_name}"  
-}
-
-# making the s3 bucket private 
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  acl    = "private"
-}
